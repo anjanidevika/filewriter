7bfc3111e1e583b8b50ee353bc745c6aa30a4d09
anjani devika <anjani devika@ANJANIDEVIKA>
2023-04-11 13:18:49 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+namespace writer
+{
+
+ class Filewriter
+        {
+
+            static void Main()
+            {
+                
+                StreamWriter sw = new StreamWriter("C://Users//anjani devika//Documents//doc1.txt");
+                Console.WriteLine("Enter the Text to write on File");
+                string str = Console.ReadLine();
+                sw.WriteLine(str);
+                sw.Flush();
+                sw.Close();
+            
+            }
+        }
+   
+}
